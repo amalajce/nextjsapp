ce72c5f2692988c366ded09c6413e4981a08ddeb
unknown <admin@T54.mca.com>
2023-05-23 14:43:42 +0530

    app updated

--- pages/countries.js
+++ pages/countries.js
@@ -1,21 +1,21 @@
 function CountryList({ countries }) {
     return (
         <div>
-            <h1> List of Books </h1>
+            <h1> List of countries </h1>
             <table>
                 <tr> 
                     <th>Code</th>
-                    {/* <th>Country</th>
+                    <th>Country</th>
                     <th>Currency</th>
-                    <th>Population</th> */}
+                    <th>Population</th>
                 </tr>
                 {countries.map(country => {
                     return (
                         <tr>
-                            <td>{country.title}</td>
-                            {/* <td>{country.countryName}</td>
+                            <td>{country.countryCode}</td>
+                            <td>{country.countryName}</td>
                             <td>{country.currencyCode}</td>
-                            <td>{country.population}</td> */}
+                            <td>{country.population}</td>
                         </tr>
                     )
                 })}
@@ -27,11 +27,11 @@ function CountryList({ countries }) {
 export default CountryList
 
 export async function getStaticProps() {
-    const response = await fetch('http://localhost:3000/api/hello')
+    const response = await fetch('https://cdn.jsdelivr.net/gh/amalajce/nextjs@main/countries.json')
     const data = await response.json()
     return {
         props: {
-            countries: data.books
+            countries: data.countries.country
         }
     }
 }
